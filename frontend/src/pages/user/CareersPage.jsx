5349555c636e907da247f16099ec062883d36474
Arshpreet <avibedi1234@gmail.com>
2023-08-13 01:58:26 +0530

    testimonial commented

--- frontend/src/pages/user/CareersPage.jsx
+++ frontend/src/pages/user/CareersPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../components/user/jsx/Header";
 import Carreer from '../../components/About/jsx/Carreer';
 import Join from "../../components/About/jsx/Join";
+import HomeBanner7 from "../../components/user/jsx/HomeBanner7";
 import Footer from "../../components/user/jsx/Footer";
 
 const CareersPage = () => {
@@ -10,6 +11,7 @@ const CareersPage = () => {
             <Header/>
             <Carreer/>
             <Join/>
+            <HomeBanner7/>
             <Footer/>
         </div>
      );
